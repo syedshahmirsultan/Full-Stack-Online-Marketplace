40d853d135b3e84775ee5a5c7692240a7c6315c6
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-06-21 17:41:03 +0500

    updated code

--- app/components/TopLevel.tsx
+++ app/components/TopLevel.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const TopLevel = () => {
     return (
-        <div className="w-full flex flex-col gap-y-4 lg:flex-row lg:justify-between lg:pr-8 lg:pl-8 lg:items-center h-auto lg:h-[56px] bg-slate-900">
-            <p className='text-gray-100 text-md'>
+        <div className="w-full flex flex-col gap-y-4 lg:flex-row justify-center lg:justify-between lg:pr-8 lg:pl-8 items-center h-[56px] bg-slate-900">
+            <p className='text-gray-100 text-sm lg:text-[16px]'>
             Free shipping on all orders over $50
             </p>
-            <div className='flex gap-x-8'>
-            <text className='text-md text-gray-100 cursor-pointer'>SignIn</text>
-            <text className='text-md text-gray-100 cursor-pointer'>SignUp</text>
+            <div className='hidden lg:flex lg:gap-x-[26px] lg:mr-2'>
+            <text className='text-sm text-gray-100 cursor-pointer uppercase'>Account</text>
+            <text className='text-sm text-gray-100 cursor-pointer uppercase'>Help Center</text>
             </div>
             
         </div>
