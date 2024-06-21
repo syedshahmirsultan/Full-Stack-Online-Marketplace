cf167890de881bb955c006bafdbd763287414c21
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-06-21 18:47:41 +0500

    added Navbar code

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { IoSearchOutline } from "react-icons/io5";
+import { FaTruck } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
+import Link from 'next/link';
+
 
 const NavBar = () => {
     return (
-        <section className="bg-white p-8 lg:max-w-7xl h-24 flex lg:gap-x-8 gap-x-4 lg:items-center">
-            <h1 className='tracking-wider text-gray-950 text-4xl lg:text-5xl'>TopShop</h1>
-            
-        </section>
+        <section className="bg-white pl-8  lg:max-w-8xl mx-auto h-24 flex gap-x-16 items-center">
+           <Link href="/"> <h1 className='tracking-tight text-gray-950 text-3xl font-semibold lg:text-4xl'>TopShop</h1>
+           </Link> <div className='h-[38px] w-6/12 border border-sky-200 hidden lg:flex place-items-center p-4 rounded-lg'>
+           <IoSearchOutline size={22} />
+           <input type="text" placeholder='Search Product by Name ...' className="h-8 w-96 ml-4 text-gray-800 outline-none"/>
+           </div>
+           <div className='hidden lg:flex cursor-pointer'>
+           <FaTruck size={34} color={'black'} className='font-bold self-center' />
+<div className='ml-4'>
+<p className='text-gray-900'>Fast Dispatch</p>
+<p className="text-slate-500">Get your order in 2-3 days</p>
+</div>
+           </div>
+           <div className="flex items-center">
+           <FiShoppingCart size={28} color={'black'}  className='self-center cursor-pointer'/>
+           </div></section>
     );
 }
 
--- app/components/TopLevel.tsx
+++ app/components/TopLevel.tsx
@@ -1,5 +1,6 @@
+import Link from 'next/link';
 import React from 'react';
-
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 const TopLevel = () => {
     return (
         <div className="w-full flex flex-col gap-y-4 lg:flex-row justify-center lg:justify-between lg:pr-8 lg:pl-8 items-center h-[56px] bg-slate-900">
@@ -7,9 +8,9 @@ const TopLevel = () => {
             Free shipping on all orders over $50
             </p>
             <div className='hidden lg:flex lg:gap-x-[26px] lg:mr-2'>
-            <text className='text-sm text-gray-100 cursor-pointer uppercase'>Account</text>
-            <text className='text-sm text-gray-100 cursor-pointer uppercase'>Help Center</text>
-            </div>
+            <LoginLink className='text-sm text-gray-100 cursor-pointer uppercase'>Account</LoginLink>
+            <Link href="/"><text className='text-sm text-gray-100 cursor-pointer uppercase'>Help Center</text>
+            </Link></div>
             
         </div>
     );
