504b815d0dcd4241dff25377ef04bc475ca5e4bf
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-06-21 17:51:14 +0500

    added navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <div>
+        <section className="bg-white p-8 lg:max-w-7xl h-24 flex lg:gap-x-8 gap-x-4 lg:items-center">
+            <h1 className='tracking-wider text-gray-950 text-4xl lg:text-5xl'>TopShop</h1>
             
-        </div>
+        </section>
     );
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import TopLevel from "./components/TopLevel";
+import NavBar from "./components/NavBar";
 
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <TopLevel/>
+        <NavBar/>
         {children}</body>
     </html>
   );
