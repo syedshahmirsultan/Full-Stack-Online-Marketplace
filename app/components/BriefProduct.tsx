742e7c68f42b3b99812995132df2549b7e79b087
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-06-29 14:52:57 +0500

    updated code

--- app/components/BriefProduct.tsx
+++ app/components/BriefProduct.tsx
@@ -12,18 +12,14 @@ import { KindeUser } from '@kinde-oss/kinde-auth-nextjs/types';
 import singleProductType from '@/types';
 
 const BriefProduct = ({ product, user }: { product: singleProductType, user: KindeUser | null }) => {
-    const { inputValue, setInput } = useInputContext();
+    const { inputValue } = useInputContext();
 
-    const handleInputChange = (value: number) => {
-        setInput(value);
-    };
 
     const { toast } = useToast();
 
     async function handleAddToCart() {
         if (user) {
-            console.log("User Object:", user);
-            console.log("User ID:", user.id); // Log user.id
+
             try {
                 await addToCartApiCall(user.id, product._id, inputValue);
                 toast({
@@ -47,7 +43,7 @@ const BriefProduct = ({ product, user }: { product: singleProductType, user: Kin
     }
 
     return (
-        <section className="w-full p-6 md:p-0 md:max-w-8xl m-2 md:ml-20 mt-20 mb-40 overflow-x-hidden">
+        <section className="w- p-6 md:p-0 md:max-w-7xl m-2 md:ml-20 mt-20 mb-40 overflow-x-hidden">
             <div className="flex flex-col md:flex-row md:gap-x-8 gap-y-2 text-center md:text-start">
                 <div>
                     <Image src={urlForImage(product.image[0])} width={650} height={600} alt={product.productname} className='w-[95%] h-80 mx-auto md:w-96 md:h-96 object-fill' />
@@ -56,12 +52,7 @@ const BriefProduct = ({ product, user }: { product: singleProductType, user: Kin
                     <h2 className='font-bold text-4xl'>{product.productname}</h2>
                     <p className='text-xl text-gray-600'>Sold by <span className='text-gray-800 font-bold text-xl'>{product.sellername}</span></p>
                     <text className="text-2xl font-extrabold text-gray-900">${product.price}</text>
-                        {/* <div className="flex flex-col gap-y-2">
-                            <p className='text-gray-700 font-bold text-lg'>Quantity</p>
-                            <div className="flex w-32 h-8 bg-white border border-gray-400 rounded-lg self-center md:self-start">
-                                <input type='number' value={inputValue} onChange={(e) => handleInputChange(Number(e.target.value))} className='outline-none ml-2 w-[90%] h-full text-gray-900 text-md' />
-                            </div>
-                        </div> */}
+                       
                     <button onClick={handleAddToCart} className='p-3 mt-6 bg-gray-950 w-44 h-12 text-white rounded-md text-md font-semibold'>Add to Cart</button>
                     <div className="flex gap-x-4 md:gap-x-8 mt-8">
                         <div className="flex flex-col gap-y-2">
@@ -80,7 +71,7 @@ const BriefProduct = ({ product, user }: { product: singleProductType, user: Kin
                 </div>
             </div>
 
-            <div className="mt-8 p-4 md:p-8">
+            <div className="mt-8 p-4 md:p-8 overflow-x-hidden">
                 <h3 className="text-gray-900 font-bold text-xl text-start">Product Description</h3>
                 <p className='text-gray-900 text-lg mt-4 p-4 text-start md:pr-20'>{product.descriptionText}</p>
             </div>
--- app/components/HamBurger.tsx
+++ app/components/HamBurger.tsx
@@ -1,46 +1,14 @@
-"use client"
-import React, { useState } from 'react';
-import { RxHamburgerMenu } from "react-icons/rx";
-import { RxCross2 } from "react-icons/rx";
-import { IoSearchOutline } from "react-icons/io5";
-import Link from 'next/link';
-import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs';
-import { LogoutLink, getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import React from 'react';
+import HamBurgerComp from './HamBurgerComp';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
-const HamBurger:React.FC = async() => {
-    const [hamBurgerIsOpen,setHamBurgerIsOpen] = useState<boolean>(false)
-    const [searchVal,setSearchVal] = useState<string>("");
+const HamBurger = async() => {
     const {getUser} = getKindeServerSession();
-    const user = await getUser();
+  const user = await getUser();
     return (
-        <section>
-        <div className='flex lg:hidden'>
-        {
-            !hamBurgerIsOpen ? (<div className="p-2 border border-gray-300 rounded-lg">
-<RxHamburgerMenu size={26} color={'black'} onClick={()=> setHamBurgerIsOpen(true)}/>
-</div>
- )
-                :  (<div className="h-96 w-full absolute  flex flex-col gap-y-2 justify-center bg-white top-0 p-4 right-0 z-50">
-<div className=" w-8 h-8  rounded-full left-0 top-10 hover:bg-red-600">
-<RxCross2 size={22} className=' text-gray-900 hover:text-white ml-1 mt-1 font-semibold'
- onClick={()=> setHamBurgerIsOpen(false)} /></div>
-
-<h1 className="text-center text-gray-950 text-xl font-bold ">Menu</h1>
-<div className='h-12 w-84 border border-gray-200 rounded-lg flex gap-x-4 items-center'>
-<Link href={`/search?query=${searchVal}`}><IoSearchOutline size={24} className='ml-2' /> </Link>
-<input type='text' placeholder='Search the Product by Name ...'
- className=' w-full  flex self-center outline-none text-gray-800'
- value={searchVal}
- onChange={(e)=> setSearchVal(e.target.value)}/>
-</div>
-<Link href="/products" className="text-gray-600 text-md font-semibold p-4 mt-4 hover:bg-gray-100/50">
-Products</Link>
-{user ?(<LogoutLink className='text-md text-gray-600 cursor-pointer uppercase p-4 font-semibold hover:bg-gray-100/50'>Log out</LogoutLink>) :(<div className='flex flex-col gap-y-4'><RegisterLink className='text-md text-gray-600 cursor-pointer uppercase p-4 font-semibold hover:bg-gray-100/50'>Sign In</RegisterLink>
-            <LoginLink className='text-md text-gray-600 cursor-pointer uppercase p-4 font-semibold hover:bg-gray-100/50'>Sign Up
-            </LoginLink></div>)}
-                </div>)
-        }
-        </div></section>
+        <div>
+        <HamBurgerComp user={user}/>
+        </div>
     );
 }
 
