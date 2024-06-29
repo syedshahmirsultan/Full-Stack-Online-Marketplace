2a2f1fede0dbf9d1474a8d383ed934ff4b6db75b
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-06-29 14:23:00 +0500

    updated code

--- app/components/TopLevel.tsx
+++ app/components/TopLevel.tsx
@@ -1,16 +1,23 @@
 import Link from 'next/link';
 import React from 'react';
 import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
-const TopLevel:React.FC = () => {
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
+const TopLevel:React.FC = async() => {
+    const {getUser} = getKindeServerSession();
+    const user = await getUser();
     return (
         <div className="w-full flex flex-col gap-y-4 lg:flex-row justify-center lg:justify-between lg:pr-8 lg:pl-8 items-center h-[56px] bg-slate-900">
             <p className='text-gray-100 text-sm lg:text-[16px]'>
             Free shipping on all orders over $50
             </p>
             <div className='hidden lg:flex lg:gap-x-[26px] lg:mr-2'>
-            <RegisterLink className='text-sm text-gray-100 cursor-pointer uppercase'>Sign In</RegisterLink>
+            {user ? (<LogoutLink className='text-sm text-gray-100 cursor-pointer uppercase'>Log out</LogoutLink>)
+            :(<div><RegisterLink className='text-sm text-gray-100 cursor-pointer uppercase'>Sign In</RegisterLink>
             <LoginLink className='text-sm text-gray-100 cursor-pointer uppercase'>Sign Up
-            </LoginLink></div>
+            </LoginLink></div>)}
+            </div>
             
         </div>
     );
